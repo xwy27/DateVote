739e52560f0f78e2188ce71c25a68025c3e25048
xwy27 <xuwy27@mail2.sysu.edu.cn>
2019-02-08 16:55:14 +0800

    fix update bug

--- bin/controllers/download.js
+++ bin/controllers/download.js
@@ -1,8 +1,23 @@
 const send = require('koa-send');
+const fs = require('fs');
+const PATH = require('path');
 const log = require('../middleware/logger');
 
 let download = async ctx => {
   log(`[Download] Downloading data...`);
+  
+  let data = require('./submit').data;
+  fs.writeFileSync(PATH.resolve(__dirname, '../../data.txt'), '');
+  for (let key in data) {
+    let people = '';
+    if (data[key].size) {
+      data[key].forEach(ele => {
+        people += ele + ' ';
+      });
+      fs.appendFileSync(PATH.resolve(__dirname, '../../data.txt'), `${key} ${data[key].size} ${people}\r\n`);
+    }
+  }
+  
   let path = `./data.txt`;
   log(`[Download] Download successfully`);
   ctx.attachment(path);
--- bin/controllers/submit.js
+++ bin/controllers/submit.js
@@ -1,9 +1,34 @@
 const log = require('../middleware/logger');
 const fs = require('fs');
 const path = require('path');
+const readline = require('readline');
 
-let storage = {};
-let data = {};
+var storage = {};
+var data = {};
+
+// load data
+(function load() {
+  let file = fs.createReadStream(path.resolve(__dirname, '../../storage.txt'));
+  let fileLines = readline.createInterface({
+    input: file
+  });
+  fileLines.on('line', line => {
+    let name = line.split(' ')[0];
+    let date = line.split(' ')[1];
+    storage[name] = date;
+    let temp = date.split(',');
+    for (t in temp) {
+      if (!data.hasOwnProperty(temp[t])) data[temp[t]] = new Set();
+      data[temp[t]].add(name);
+    }
+  });
+
+  fileLines.on('close', () => {
+    // console.log(storage);
+    // console.log(data);
+    console.log('Load End');
+  });
+}());
 
 let submit = async ctx => {
   let body = ctx.request.body;
@@ -15,28 +40,67 @@ let submit = async ctx => {
   log(`Time: ${time.toUTCString()} [${msg}] Name:${name} Date:${date}`);
 
   msg += ' Successfully';
-  storage[name] = date;
-
+  let prev = storage[name];
   let temp = date.split(',');
-  for (t in temp) {
-    if (!data.hasOwnProperty(temp[t])) data[temp[t]] = new Set();
-    data[temp[t]].add(name);
-  }
-
-  fs.writeFileSync(path.resolve(__dirname, '../../data.txt'), '');
-  for (let key in data) {
-    let people = '';
-    data[key].forEach(ele => {
-      people += ele + ' ';
+  if (prev === undefined) { // first time
+    for (t in temp) {
+      if (!data.hasOwnProperty(temp[t])) data[temp[t]] = new Set();
+      data[temp[t]].add(name);
+    }
+  } else {  // update
+    prev = prev.split(',');
+    let del = [];
+    let add = [];
+    for (let i = 0; i < prev.length; ++i) {
+      let flag = false;
+      for (let j = 0; j < temp.length; ++j) {
+        if (temp[j] === prev[i]) {
+          break;
+        } else if (j === temp.length - 1) {
+          flag = true;
+        }
+      }
+      if (flag) {
+        del.push(prev[i]);
+      }
+    }
+    
+    for (let i = 0; i < temp.length; ++i) {
+      let flag = false;
+      for (let j = 0; j < prev.length; ++j) {
+        if (temp[i] === prev[j]) {
+          break;
+        } else if (j === prev.length - 1) {
+          flag = true;
+        }
+      }
+      if (flag) {
+        add.push(temp[i]);
+      }
+    }
+    
+    del.forEach(x => {
+      data[x].delete(name);
     });
-    fs.appendFileSync(path.resolve(__dirname, '../../data.txt'), `${key} ${data[key].size} ${people}\r\n`);
-  }
 
+    add.forEach(x => {
+      if (!data.hasOwnProperty(x)) data[x] = new Set();
+      data[x].add(name);
+    });
+  }
+  
+  storage[name] = date;
+  fs.writeFileSync(path.resolve(__dirname, '../../storage.txt'), '');
+  for (let key in storage) {
+    fs.appendFileSync(path.resolve(__dirname, '../../storage.txt'), `${key} ${storage[key]}\r\n`);
+  }
+  
   ctx.body = {
     msg: msg
   };
 }
 
 module.exports = {
-  'POST /submit': submit
+  'POST /submit': submit,
+  'data': data
 }
